878ade970e7a0610b7ea7a36a9c6e6141deac7e5
rnkorneeva <rnkorneeva@gmail.com>
2018-07-18 17:08:23 +0300

    s com

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -9,5 +9,22 @@
 import Foundation
 
 struct Card: Hashable {
+    var hashValue: Int { return identifier }
     
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    var isFacedUp = false
+    var isMatched = false
+    private var identifier: Int
+    
+    private static var identifierFactory = 0
+    
+    private static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
 }
